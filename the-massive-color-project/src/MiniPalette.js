738c3a5c50164ab2b76d8968a51c097fb1306962
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-10-28 10:19:04 +0300

    feat: style paletteList with jss

---
+++ the-massive-color-project/src/MiniPalette.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+  root: {
+    backgroundColor: '#fff',
+    borderRadius: '5px',
+    position: 'relative',
+    padding: '0.5rem',
+    overflow: 'hidden',
+    border: '1px solid #000',
+    '&:hover': {
+      cursor: 'pointer'
+    }
+  },
+  colors: {
+    backgroundColor: 'grey'
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    color: '#000',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+    position: 'relative'
+  },
+  emoji: {
+    marginLeft: '0.5rem',
+    fontSize: '1.5rem'
+  }
+};
+
+function MiniPalette(props) {
+  const {classes, paletteName, emoji} = props;
+  return(
+    <div className={classes.root}>
+      <div className={classes.colors}>
+
+      </div>
+      <h5 className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></h5>
+    </div>
+  );
+}
+
+export default withStyles(styles)(MiniPalette);
